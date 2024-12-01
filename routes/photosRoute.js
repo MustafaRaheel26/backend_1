2e8318778487d64c9afd8823e5d093c420ddca50
Mustafa <153886730+MustafaRaheel26@users.noreply.github.com>
2024-12-01 23:59:21 +0500

    Add files via upload

--- routes/photosRoute.js
+++ routes/photosRoute.js
@@ -7,6 +7,11 @@ photosRoute.get('/',(req,res) =>{
        res.status(200).send({status:200,message:"success",data:photos})
 })
 
+photosRoute.get('/:id',(req,res) =>{
+    res.status(200).send({status:200,message:"success",data:[]})
+})
+
+
 // postsRoute.get('/:id',(req,res) =>{
 //     res.status(200).send({status:200,message:"success",data:[]})
 // })
